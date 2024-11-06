59b04e5518b6efb89d2c6a6f80b78271e0e36bc5
Brian J. Cardiff <bcardiff@gmail.com>
2024-11-06 18:44:41 -0300

    showJuego

--- src/CLI.hs
+++ src/CLI.hs
@@ -1,4 +1,23 @@
 module CLI (main) where
 
+import CuatroEnLinea
+
 main :: IO ()
-main = putStrLn "someFunc"
+main =
+  let j0 = nuevo
+      (j1, r1) = poner Rojo 1 j0
+      (j2, r2) = poner Amarillo 1 j1
+   in putStrLn (showJuego j2)
+
+showJuego :: Juego -> String
+showJuego j =
+  let css = casillas j
+   in concat (map (\f -> showFila f ++ "\n") css)
+
+showFila :: [Maybe Color] -> String
+showFila l = concat (map showColor l)
+
+showColor :: Maybe Color -> String
+showColor Nothing = "⚪️"
+showColor (Just Amarillo) = "🟡"
+showColor (Just Rojo) = "🔴"
